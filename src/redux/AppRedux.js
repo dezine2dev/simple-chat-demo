8514aec0983dca67b345b50a97fc6b7b93190a8b
Isa <isanoguchi91@gmail.com>
2020-03-31 12:25:49 -0500

    Basic App redux with msgs state

---
+++ src/redux/AppRedux.js
@@ -0,0 +1,40 @@
+import { createReducer, createActions } from 'reduxsauce';
+import shortid from 'shortid';
+
+/* --------------------- Types and Action Creators ---------------- */
+const { Types, Creators } = createActions({
+  setMsgs: ['msg']
+});
+
+export const AppTypes = Types;
+
+export default Creators;
+
+/* --------------------- Selectors ---------------- */
+export const AppSelectors = {
+  selectMsgs: state => state.app.msgs
+};
+
+/* --------------------- Initial State ----------------- */
+export const INITIAL_STATE = {
+  msgs: []
+};
+
+/* ------------------- Reducers --------------------- */
+const setMsgs = (state, { msg }) => {
+  const newMsg = {
+    id: shortid.generate(),
+    timestamp: Date.now(),
+    msg
+  };
+  const extendedMsgs = [...state.msgs, newMsg];
+
+  return {
+    ...state,
+    msgs: extendedMsgs
+  };
+};
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.SET_MSGS]: setMsgs
+});
